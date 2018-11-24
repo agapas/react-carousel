0b47ae2bb9f24673aafb8c7c1f1cff67a2b7e930
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2018-11-24 14:18:26 +0000

    Installed Enzyme for unit tests

--- src/test/carousel-app.test.tsx
+++ src/test/carousel-app.test.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import { CarouselApp } from '../components/carousel-app';
 
-// TODO: instal enzyme to use shallow rendering for testing components
-
 describe('CarouselApp', () => {
-  it('should render without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<CarouselApp imagesUrls={[]} imagesSource={[]} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  it('renders without crashing', () => {
+    shallow(<CarouselApp />);
   });
 });
